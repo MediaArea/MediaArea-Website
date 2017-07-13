8cd678e60e5a8825bb168f3cad7b2d128cffb6a7
Guillaume Roques <guillaume.roques@gmail.com>
2017-07-13 16:39:47 +0200

    Add basic admin interface for users management
    Signed-off-by: Guillaume Roques <guillaume.roques@gmail.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new MediaTraceBundle\MediaTraceBundle(),
             new BWFMetaEditBundle\BWFMetaEditBundle(),
             new QCToolsBundle\QCToolsBundle(),
