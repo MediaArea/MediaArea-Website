938da62db2b216ab1c42bc7c34c28ef5c4f8e417
Guillaume Roques <guillaume.roques@gmail.com>
2017-10-13 10:16:44 +0200

    Minor modifications
    Signed-off-by: Guillaume Roques <guillaume.roques@gmail.com>

--- src/AppBundle/Resources/public/js/sidebar.js
+++ src/AppBundle/Resources/public/js/sidebar.js
@@ -59,58 +59,55 @@ var sidebar = (function () {
     };
 
     var scrollMenu = function() {
-        var $sidebar = $('#sidebar .sidebar-menu');
-        if ($sidebar.length > 0) {
-            var $sidebar_a = $sidebar.find('a');
+        var sidebar = $('#sidebar .sidebar-menu');
+        if (sidebar.length > 0) {
+            var sidebar_a = sidebar.find('a');
 
-            $sidebar_a
+            sidebar_a
                 .addClass('scrolly')
                 .on('click', function() {
-                    var $this = $(this);
+                    var elem = $(this);
 
                     // External link? Bail.
-                        if ($this.attr('href').charAt(0) != '#')
+                        if (elem.attr('href').charAt(0) != '#')
                             return;
 
                     // Deactivate all links.
-                        $sidebar_a.removeClass('active');
+                        sidebar_a.removeClass('active');
 
                     // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-                        $this
-                            .addClass('active')
-                            .addClass('active-locked');
+                        elem.addClass('active active-locked');
 
                 })
                 .each(function() {
 
-                    var $this = $(this),
-                        id = $this.attr('href'),
-                        $section = $(id);
+                    var elem = $(this),
+                        section = $(elem.attr('href'));
 
                     // No section for this link? Bail.
-                    if ($section.length < 1)
+                    if (section.length < 1)
                         return;
 
                     // Scrollex.
-                    $section.scrollex({
+                    section.scrollex({
                         mode: 'top',
                         top: -50,
                         bottom: 0,
                         initialize: function() {
-                            $section.addClass('inactive');
+                            section.addClass('inactive');
                         },
                         enter: function() {
                             // Activate section.
-                            $section.removeClass('inactive');
+                            section.removeClass('inactive');
 
                             // No locked links? Deactivate all links and activate this section's one.
-                            if ($sidebar_a.filter('.active-locked').length == 0) {
-                                $sidebar_a.removeClass('active');
-                                $this.addClass('active');
+                            if (sidebar_a.filter('.active-locked').length == 0) {
+                                sidebar_a.removeClass('active');
+                                elem.addClass('active');
                             }
                             // Otherwise, if this section's link is the one that's locked, unlock it.
-                            else if ($this.hasClass('active-locked'))
-                                $this.removeClass('active-locked');
+                            else if (elem.hasClass('active-locked'))
+                                elem.removeClass('active-locked');
                         }
                     });
                 });
