11767288f5e7b670e1fb089ff2cf57ff936dfd7b
Guillaume Roques <guillaume.roques@gmail.com>
2018-11-22 11:31:06 +0100

    MediaConchOnline: fix rules validators MediaArea/MediaConchOnline#95
    Signed-off-by: Guillaume Roques <guillaume.roques@gmail.com>

--- src/MediaConchOnlineBundle/Lib/XslPolicy/XslPolicyFormFields.php
+++ src/MediaConchOnlineBundle/Lib/XslPolicy/XslPolicyFormFields.php
@@ -10,31 +10,31 @@ class XslPolicyFormFields
     public static function getValidators()
     {
         return [
-            'is_equal' => 'Is equal (==)',
-            'is_not_equal' => 'Is not equal (!=)',
-            'is_less_than' => 'Is less than (<)',
-            'is_less_or_equal_than' => 'Is less or equal than (<=)',
-            'is_greater_than' => 'Is greater than (>)',
-            'is_greater_or_equal_than' => 'Is greater or equal than (>=)',
-            'exists' => 'Exists',
-            'does_not_exist' => 'Does not exist',
-            'contains_string' => 'Contains string',
+            'Is equal (==)' => 'is_equal',
+            'Is not equal (!=)' => 'is_not_equal',
+            'Is less than (<)' => 'is_less_than',
+            'Is less or equal than (<=)' => 'is_less_or_equal_than',
+            'Is greater than (>)' => 'is_greater_than',
+            'Is greater or equal than (>=)' => 'is_greater_or_equal_than',
+            'Exists' => 'exists',
+            'Does not exist' => 'does_not_exist',
+            'Contains string' => 'contains_string',
         ];
     }
 
     public static function getOperators()
     {
         return [
-            'exists' => 'Exists',
-            'must not exist' => 'Does not exist',
-            '=' => 'Is equal (=)',
-            '!=' => 'Is not equal (!=)',
-            '<' => 'Is less than (<)',
-            '<=' => 'Is less or equal than (<=)',
-            '>' => 'Is greater than (>)',
-            '>=' => 'Is greater or equal than (>=)',
-            'starts with' => 'Starts with',
-            'must not start with' => 'Does not start with',
+            'Exists' => 'exists',
+            'Does not exist' => 'must not exist',
+            'Is equal (=)' => '=',
+            'Is not equal (!=)' => '!=',
+            'Is less than (<)' => '<',
+            'Is less or equal than (<=)' => '<=',
+            'Is greater than (>)' => '>',
+            'Is greater or equal than (>=)' => '>=',
+            'Starts with' => 'starts with',
+            'Does not start with' => 'must not start with',
         ];
     }
 
